5591833fa971c9f1cbf0c1dd23bf94f0d85a279b
XsamaVN <buianhduc97@gmail.com>
2023-10-25 21:35:41 +0700

    done

--- signInAndSignUp/codeJS.js
+++ signInAndSignUp/codeJS.js
@@ -1,49 +1,87 @@
-// let danhSachTaiKhoan = JSON.parse(localStorage.getItem('taiKhoan')) || [];
-let accountDN = document.getElementById('taiKhoanDangNhap').value
-let passDN = document.getElementById('taiKhoanDangNhap').value
-let accountDK = document.getElementById('taiKhoanDangKy').value;
-let passDK = document.getElementById('matKhauDangKy').value;
-let rePassDK = document.getElementById('nhapLaiMatKhauDangKy').value;
 
-
-
-    let nutDK = document.querySelector("#nutDK");
-    nutDK.addEventListener("click", (e) => {
-        e.preventDefault();
-        if (accountDK === "" || passDK === "" || rePassDK === '') {
-            alert("vui lòng không để trống");
+function signUp() {
+    let taiKhoanDK = document.getElementById('taiKhoanDangKy').value;
+    let matKhauDK = document.getElementById('matKhauDangKy').value;
+    let rePassDK = document.getElementById('nhapLaiMatKhauDangKy').value;
+    // Kiểm tra xem Local Storage có sẵn không
+    if (typeof(Storage) !== "undefined") {
+        if(matKhauDK !== rePassDK){
+            document.getElementById('nhapsai').innerHTML = 'mật khẩu nhập lại không khớp'
         }
         else {
+       if(taiKhoanDK.length < 6 && matKhauDK.length < 6){
+           document.getElementById('nhapsai').innerHTML = 'tài khoản mà mật khẩu phải có nhiều hơn 6 ký tự'
+       }
+       else {
+           // Lấy dữ liệu từ Local Storage (nếu có)
+           let danhSachTaiKhoan = JSON.parse(localStorage.getItem('danhSachTaiKhoan')) || [];
 
-            let user = {
-                username: accountDK,
-                password: passDK,
-            };
-            let json = JSON.stringify(user);
-            localStorage.setItem(accountDK, json);
-            alert("Đăng Ký Thành Công");
-            window.location.href = "login.html";
+           // Thêm tài khoản mới vào danh sách
+           danhSachTaiKhoan.push({ taiKhoan: taiKhoanDK, matKhau: matKhauDK });
+
+           // Lưu lại danh sách tài khoản vào Local Storage
+           localStorage.setItem('danhSachTaiKhoan', JSON.stringify(danhSachTaiKhoan));
+           alert('Đăng ký thành công');
+           window.location.href = "login.html"
+       }
         }
-        // if(passDK.value !== rePassDK){
-        //     document.getElementById('nhapsai').innerHTML='Mật khẩu nhập lại không khớp'
-        // }
-    });
+    } else {
+        alert("Trình duyệt của bạn không hỗ trợ Local Storage.");
+    }
 
+}
 
+function signIn() {
+    let taiKhoanDN = document.getElementById('taiKhoanDangNhap').value;
+    let matKhauDN = document.getElementById('matKhauDangNhap').value;
+    let dangNhapThanhCong = false;
+    // Kiểm tra xem Local Storage có sẵn không
+    if (typeof(Storage) !== "undefined") {
+        if(taiKhoanDN.length < 6 && matKhauDN.length < 6){
+                document.getElementById('chuaNhap').innerHTML = 'tài khoản và mật khẩu phải có nhiều hơn 6 ký tự'
+            }
+            else {
+            // Lấy danh sách tài khoản từ Local Storage
+            let danhSachTaiKhoan = JSON.parse(localStorage.getItem('danhSachTaiKhoan')) || [];
 
-        let nutDN = document.querySelector("#phimDN");
-        nutDN.addEventListener("click", (e) => {
-            e.preventDefault();
-            if (accountDN === "" || passDN === "") {
-                alert("vui lòng không để trống");
-            } else {
-                let user = JSON.parse(localStorage.getItem(accountDN));
-                if (user.username === accountDN && user.password === passDN) {
-                    alert("Đăng Nhập Thành Công");
-                    window.location.href = "index.html";
-                } else {
-                    alert("Đăng Nhập Thất Bại");
+            // Kiểm tra xem tài khoản và mật khẩu có tồn tại trong danh sách không
+
+            for (let i = 0; i < danhSachTaiKhoan.length; i++) {
+                if (danhSachTaiKhoan[i].taiKhoan === taiKhoanDN && danhSachTaiKhoan[i].matKhau === matKhauDN) {
+                    dangNhapThanhCong = true;
+                    break;
                 }
             }
-        });
+        }
+
+
+        if (dangNhapThanhCong){
+           alert("Đăng nhập thành công!")
+            window.location.href = "user.html";
+        } else {
+            document.getElementById('chuaNhap').innerHTML = "Tài khoản hoặc mật khẩu không đúng.";
+        }
+        if(taiKhoanDN === 'admin1' && matKhauDN === 'admin1'){
+            alert('chào mừng Admin đăng nhập')
+            window.location.href = 'index.html'
+        }
+    } else {
+        alert("Trình duyệt của bạn không hỗ trợ Local Storage.");
+    }
+}
+
+function kiemTraNhap() {
+    let taiKhoanDN = document.getElementById('taiKhoanDangNhap').value;
+    let matKhauDN = document.getElementById('matKhauDangNhap').value;
+    if(taiKhoanDN.length < 6 || matKhauDN.length < 6){
+        document.getElementById('chuaNhap').innerHTML = 'tài khoản và mật khẩu phải có nhiều hơn 6 ký tự'
+    }
+    else if(taiKhoanDN.length === 0 && matKhauDN.length === 0){
+        document.getElementById('chuaNhap').innerHTML = '&nbsp;'
+    }
+    else {
+            document.getElementById('chuaNhap').innerHTML = '&nbsp;'
+    }
+}
+
 
---
+++ signInAndSignUp/codeQLSP.js
@@ -0,0 +1,73 @@
+let danhSachSanPham = JSON.parse(localStorage.getItem('x'))|| [];
+
+function list() {
+    // let danhSachSanPham = JSON.parse(localStorage.getItem('x'))
+    let str = '<h1>Danh sách sản phẩm</h1>';
+    str += `<p>Số lượng sản phẩm: ${danhSachSanPham.length} </p>`;
+    for (let i = 0; i < danhSachSanPham.length; i++) {
+        str += `<h4>${danhSachSanPham[i].sanpham}  <button onclick="sua(${i})">Sửa</button> <button onclick="xoa(${i})">Xóa</button></h4>`
+    }
+    document.getElementById('hienThi').innerHTML = str;
+}
+
+function listMember() {
+    // let danhSachSanPham = JSON.parse(localStorage.getItem('x'))
+    let str = '<h1>Danh sách sản phẩm</h1>';
+    str += `<p>Số lượng sản phẩm: ${danhSachSanPham.length} </p>`;
+    for (let i = 0; i < danhSachSanPham.length; i++) {
+        str += `<h4>${danhSachSanPham[i].sanpham}`
+    }
+    document.getElementById('hienThi').innerHTML = str;
+}
+function them() {
+    document.getElementById('hienThi').innerHTML = '<input id="nhap"> <button onclick="taoMoi()">Tạo mới</button>';
+
+}
+
+function taoMoi() {
+
+    let sanPham = document.getElementById('nhap').value
+    // Thêm tài khoản mới vào danh sách
+    danhSachSanPham.push({sanpham: sanPham});
+    // Lưu lại danh sách tài khoản vào Local Storage
+    localStorage.setItem('x',JSON.stringify(danhSachSanPham));
+    list();
+}
+
+function sua(i) {
+    document.getElementById('hienThi').innerHTML = `<input  id="nhap" value = "${danhSachSanPham[i]}"> <button onclick="luu(${i})">Lưu</button>`;
+}
+
+function luu(i) {
+    danhSachSanPham[i].sanpham = document.getElementById('nhap').value;
+    list();
+}
+
+function xoa(i) {
+    danhSachSanPham.splice(i,1);
+    localStorage.setItem('x',JSON.stringify(danhSachSanPham));
+    list();
+}
+
+function tim() {
+    let str = '<input  id="timKiem" oninput="search()">' //<button onclick="search()">Tìm</button>
+    document.getElementById('hienThi').innerHTML = str + '<div id="hienThi2"></div>';
+}
+
+function search() {
+    let giaTri = document.getElementById('timKiem').value
+    let dem = 0
+    let search = []
+    for (let i = 0; i < danhSachSanPham.length; i++) {
+        let kiemTra = danhSachSanPham[i].sanpham.includes(giaTri)
+        if (kiemTra) {
+            dem++
+            search.push(danhSachSanPham[i].sanpham)
+        }
+    }
+    if (dem !== 0) {
+        document.getElementById('hienThi2').innerHTML = 'Các sản phẩm tìm thấy: <br>' + search.join('<br>')
+    } else {
+        document.getElementById('hienThi2').innerHTML = 'không tìm thấy sản phẩm nào'
+    }
+}
